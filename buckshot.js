da81fbbe85cd8c81a8dba44c6f28a5a1eef62e32
jeremystone4321 <jermeystone4321@gmail.com>
2020-11-27 09:39:52 -0700

    obstacles, barricades, sfx engine

---
+++ buckshot.js
@@ -0,0 +1,3 @@
+var buckshotsfx = []
+		playlistmaker(buckshotsfx, 6, "buckshotshot.mp3", 1, 0.2, 0);
+		var buckshotplaylist = 0;
---
+++ evilbulletworker.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+function drawevilbullets(evilbullets) {
+	const canvas = new OffScreenCanvas(1280, 720);
+	const ctx = canvas.getContext("2d");
+
+	console.log("HEY");
+	//evilbullets.drawme();
+
+//	return evilbullets;
+}
+
+
